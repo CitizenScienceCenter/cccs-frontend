c2258cf24f8981a204165986241be5f7d0253ee8
encima <encima@gmail.com>
2018-05-09 09:45:29 +0200

    intiial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="login-wrapper border border-light">
+    <h1>{{msg}}</h1>
+    <form class="form-signin" @submit.prevent="login">
+      <h2 class="form-signin-heading">Please sign in</h2>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @submit.prevent="login">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Please login here',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.email)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
