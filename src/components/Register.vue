eabf3e68d86b3cb621f829c8984b42c3b1650cb8
encima <encima@gmail.com>
2018-05-09 12:22:02 +0200

    added signup to vue application

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="login-wrapper border border-light">
-    <form class="form-signin" @submit.prevent="login">
+    <form class="form-signin" @submit.prevent="register">
       <h2 class="form-signin-heading">{{msg}}</h2>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <button class="btn btn-lg btn-primary btn-block" type="submit" @submit.prevent="login">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @submit.prevent="login">Sign Up</button>
     </form>
   </div>
 </template>
@@ -16,16 +16,16 @@ export default {
   name: 'Login',
   data () {
     return {
-      msg: 'Please login here',
+      msg: 'Sign up for an account',
       email: '',
       password: ''
     }
   },
   methods: {
-    login () {
+    register () {
       console.log(this.email)
       this.$http
-        .post('/users/authorize', { email: this.email, pwd: this.password })
+        .post('/users/register', { email: this.email, pwd: this.password })
         .then(request => console.log(request))
         .catch((e) => console.error(e))
     }
