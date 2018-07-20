eb7fcda03abd5ea9a094ff8cb6b7cfb0a24e8339
encima <encima@gmail.com>
2018-07-20 07:50:35 -0400

    tests passing with polyfill and i18n injection for unit tests

--- test/unit/specs/Dashboard.spec.js
+++ test/unit/specs/Dashboard.spec.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import { Dashboard } from '@/views/home'
+import store from '@/store'
+import * as loc from '@/localisation/index'
+import vuexI18n from 'vuex-i18n'
+
+Vue.use(vuexI18n.plugin, store)
+Vue.i18n.add('en', loc.en)
 
 describe('Dashboard', () => {
 
